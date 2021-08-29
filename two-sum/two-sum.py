5fc11b3ae48222b4495427cd9a0e428c776e62eb
Mohana Sundaram M <64247372+ms-ai22@users.noreply.github.com>
2021-08-29 20:27:00 +0530

    Delete two-sum directory

--- two-sum/two-sum.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        master = {}
-        for idx,val in enumerate(nums):
-            if target-val in master:
-                return [idx,master[target-val]]
-            master[val] = i
