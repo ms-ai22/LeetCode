0b6df0675d086f0c19ef188c3c6f839070ebff72
Mohana Sundaram M <64247372+ms-ai22@users.noreply.github.com>
2021-08-29 20:26:23 +0530

    Easy

---
+++ 1. Two Sum/TwoSum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        master = {}
+        for idx,val in enumerate(nums):
+            if target-val in master:
+                return [idx,master[target-val]]
+            master[val] = idx
