3a2c2a8f4ca8fa3db045856236e50f515262b229
Mohana Sundaram M <64247372+ms-ai22@users.noreply.github.com>
2021-09-05 00:17:37 +0530

    Easy

--- 118. Pascal's Triangle/PascalsTriangle1.py
+++ 118. Pascal's Triangle/PascalsTriangle1.py
@@ -1,9 +1,9 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         ans = [[1]]
-        while len(ans[-1])<numRows:
+        while len(ans)<numRows:
             temp = []
-            for idx in range(1,len(ans[-1])):
+            for idx in range(1,len(ans)):
                 temp.append(ans[-1][idx-1]+ans[-1][idx])
             ans.append([1]+temp+[1])
         return ans
